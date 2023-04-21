7ab5144afadd4ae119c5c86a335169f7f7c0210b
this-is-yello <yello_3617@naver.com>
2023-04-21 19:14:45 +0900

    in C

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:rentalsuitstate/style.dart';
 
-import 'dart:async';
-import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -13,7 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: '렌탈복 상태관리',
       home: RentalSuitState(),
@@ -29,28 +39,59 @@ class RentalSuitState extends StatefulWidget {
 }
 
 class _RentalSuitStateState extends State<RentalSuitState> {
-  Future _scan() async {
-    String barcode = await scanner.scan().toString();
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        body: SearchCondition(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _scan(),
-          tooltip: 'scan',
-          child: const Icon(Icons.qr_code_rounded),
-          backgroundColor: blackColor,
+        body: TabBarView(
+          children: [
+            Tab(
+              child: SearchCondition(),
+            ),
+            Tab(
+              child: Container(),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          color: whiteColor,
+          child: TabBar(
+            labelColor: whiteColor,
+            unselectedLabelColor: blackColor,
+            indicator: BoxDecoration(
+              color: blackColor,
+            ),
+            tabs: [
+              Container(
+                height: 64,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.search),
+                    Text('Search'),
+                  ],
+                ),
+              ),
+              Container(
+                height: 64,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.calendar_month),
+                    Text('Calender'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+// ----------------------------------------------- Tab_1 -----------------------------------------------
 class SearchCondition extends StatefulWidget {
   const SearchCondition({super.key});
 
@@ -58,98 +99,208 @@ class SearchCondition extends StatefulWidget {
   State<SearchCondition> createState() => _SearchConditionState();
 }
 
+TextEditingController inputKeyword = TextEditingController();
+
 class _SearchConditionState extends State<SearchCondition> {
-  var _inputSearch = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 120,
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                elevation: 0,
-                pinned: true,
-                floating: true,
-                centerTitle: true,
-                automaticallyImplyLeading: false,
-                expandedHeight: 120,
-                toolbarHeight: 120,
-                backgroundColor: blackColor,
-                title: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(16),
-                      child: TextField(
-                        controller: _inputSearch,
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: whiteColor,
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: blackColor,
-                          ),
-                          hintText: '검색어를 입력하세요.',
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 2,
-                              color: greyColor,
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 120,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  elevation: 0,
+                  pinned: true,
+                  floating: true,
+                  centerTitle: true,
+                  automaticallyImplyLeading: false,
+                  expandedHeight: 120,
+                  toolbarHeight: 120,
+                  backgroundColor: blackColor,
+                  title: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(16),
+                        child: TextField(
+                          controller: inputKeyword,
+                          onChanged: (_inputSearch) {
+                            print(_inputSearch);
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: whiteColor,
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: blackColor,
                             ),
-                            borderRadius: BorderRadius.circular(8),
+                            hintText: '검색어를 입력하세요.',
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(width: 2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: greyColor,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(16),
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return InkWell(
-                  child: Container(
-                    width: double.infinity,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: greyColor,
-                          strokeAlign: BorderSide.strokeAlignInside,
-                          width: 2,
-                        ),
-                      ),
-                    ),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Num / Size(Type)',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: blackColor,
-                          ),
-                        ),
-                      ],
-                    ),
+                    ],
                   ),
-                  onTap: () {},
-                );
-              },
+                ),
+              ],
             ),
           ),
-        )
-      ],
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    child: Container(
+                      width: double.infinity,
+                      height: 72,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: greyColor,
+                            strokeAlign: BorderSide.strokeAlignInside,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Num / Size(Type)',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: blackColor,
+                            ),
+                          ),
+                          Text(
+                            'State',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: blackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {},
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const QRViewExample(),
+          ));
+        },
+        backgroundColor: blackColor,
+        child: const Icon(
+          Icons.qr_code_rounded,
+        ),
+      ),
     );
   }
 }
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? qrController;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      qrController!.pauseCamera();
+    }
+    qrController!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: _buildQrView(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController qrController) {
+    setState(() {
+      this.qrController = qrController;
+    });
+    qrController.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+        inputKeyword.text = scanData.toString();
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    qrController?.dispose();
+    super.dispose();
+  }
+}
+
+// ----------------------------------------------- Tab_2 -----------------------------------------------
--- lib/style.dart
+++ lib/style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-var mainColor = Color(0xFF205B48);
-var blackColor = Color(0xFF1E1E1E);
-var whiteColor = Color(0xFFFFFFFF);
-var greyColor = Color(0xFF9E9E9E);
+var mainColor = const Color(0xFF205B48);
+var blackColor = const Color(0xFF1E1E1E);
+var whiteColor = const Color(0xFFFFFFFF);
+var greyColor = const Color(0xFF9E9E9E);
